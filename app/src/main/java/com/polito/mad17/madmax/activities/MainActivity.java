82b1a69abb989b272890731565b38d10d674c237
RiccardoDiVittorio <rikydivi@gmail.com>
2017-04-27 12:46:56 +0200

    Aggiunta la funzionalità per inviatare un amico

--- app/src/main/java/com/polito/mad17/madmax/activities/MainActivity.java
+++ app/src/main/java/com/polito/mad17/madmax/activities/MainActivity.java
@@ -4,12 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.google.android.gms.appinvite.AppInviteInvitation;
 import com.google.firebase.auth.FirebaseAuth;
 import com.polito.mad17.madmax.R;
 
@@ -18,7 +20,10 @@ public class MainActivity extends AppCompatActivity {
     private String[] drawerOptions;
     private DrawerLayout drawerLayout;
     private ListView drawerList;
+ //   private ActionBarDrawerToggle drawerToggle;
     private FirebaseAuth auth;
+    private static final int REQUEST_INVITE = 0;
+    private String TAG = ("Main_activity");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,8 +36,10 @@ public class MainActivity extends AppCompatActivity {
         drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
         drawerList = (ListView)findViewById(R.id.left_drawer);
 
+        // TODO: inserire il fragment della GroupsActivity
+
         // set the adapter for the Listview
-        drawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_list_item, drawerOptions));
+        drawerList.setAdapter(new ArrayAdapter<>(this, R.layout.drawer_list_item, drawerOptions));
 
         // set the click's listener
         drawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -46,7 +53,36 @@ public class MainActivity extends AppCompatActivity {
                     finish();
                     startActivity(intent);
                 }
+                else if(position == 0){
+                    Intent intent = new AppInviteInvitation.IntentBuilder(getString(R.string.invitation_title))
+                            .setMessage(getString(R.string.invitation_message))
+     //                       .setDeepLink(Uri.parse(getString(R.string.invitation_deep_link)))
+     //                       .setCustomImage(Uri.parse(getString(R.string.invitation_custom_image)))
+                            .setCallToActionText(getString(R.string.invitation_cta))
+                            .build();
+                    startActivityForResult(intent, REQUEST_INVITE);
+                }
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        Log.d(TAG, "onActivityResult: requestCode=" + requestCode + ", resultCode=" + resultCode);
+
+        if(requestCode == REQUEST_INVITE){
+            if(resultCode == RESULT_OK){
+                // Get the invitation IDs of all sent messages
+                String[] ids = AppInviteInvitation.getInvitationIds(resultCode, data);
+                for (String id : ids) {
+                    Log.d(TAG, "onActivityResult: sent invitation " + id);
+                }
+            } else {
+                // Sending failed or it was canceled, show failure message to the user
+                Log.d(TAG, "onActivityResult: failed sent");
+            }
+        }
     }
 }
