3699c014867f114b4738222e1fb22714a75834e2
deviz92 <valentino.davide@live.it>
2017-06-07 12:00:58 +0200

    - bugfix scelta valuta di default - sistemazione traduzioni - sistemazione notifiche settabili

--- app/src/main/java/com/polito/mad17/madmax/activities/SettingsFragment.java
+++ app/src/main/java/com/polito/mad17/madmax/activities/SettingsFragment.java
@@ -16,16 +16,8 @@ public class SettingsFragment extends PreferenceFragment {
     private static final String TAG = SettingsFragment.class.getSimpleName();
 
     public static final String NOTIFICATION_INVITE = "notification_invite";
-    public static final String NOTIFICATION_JOIN_GROUP = "notification_joinGroup";
-    public static final String NOTIFICATION_LEAVE_GROUP = "notification_leaveGroup";
     public static final String NOTIFICATION_EXPENSE = "notification_expense";
     public static final String NOTIFICATION_PROPOSAL_EXPENSE = "notification_proposalExpense";
-    public static final String NOTIFICATION_COMMENT = "notification_comment";
-    public static final String NOTIFICATION_VOTE = "notification_vote";
-    public static final String NOTIFICATION_PROPOSAL_APPROVED = "notification_proposalApproved";
-    public static final String NOTIFICATION_PROPOSAL_NEGLECTED = "notification_proposalNeglected";
-    public static final String NOTIFICATION_PAYMENT = "notification_payment";
-    public static final String NOTIFICATION_CONFIRM_PAYMENT = "notification_confirmPayment";
     public static final String DEFAULT_CURRENCY = "defaultCurrency";
 
     private DatabaseReference databaseReference;
@@ -40,17 +32,14 @@ public class SettingsFragment extends PreferenceFragment {
 
     @Override
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
-        CheckBoxPreference boxPref = (CheckBoxPreference)preference;
+        if (preference instanceof CheckBoxPreference) {
+            CheckBoxPreference boxPref = (CheckBoxPreference) preference;
 
-        if(boxPref.isChecked())
-            databaseReference.child("users").child(MainActivity.getCurrentUID()).child("notifications").child(boxPref.getKey()).setValue(true);
-        else
-            databaseReference.child("users").child(MainActivity.getCurrentUID()).child("notifications").child(boxPref.getKey()).setValue(false);
-
-        /*if(((CheckBoxPreference)preference).isChecked())
-            Log.d(TAG, "preference "+preference.getKey()+" has been activated");
-        else
-            Log.d(TAG, "preference "+preference.getKey()+" has been deactivated");*/
+            if (boxPref.isChecked())
+                databaseReference.child("users").child(MainActivity.getCurrentUID()).child("notifications").child(boxPref.getKey()).setValue(true);
+            else
+                databaseReference.child("users").child(MainActivity.getCurrentUID()).child("notifications").child(boxPref.getKey()).setValue(false);
+        }
 
         return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
